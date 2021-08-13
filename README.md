80ad5a69b5fed7a296dbbafa0c1ab909a024f98c
basuabhirup <basu.abhirup@yahoo.com>
2021-08-13 14:02:57 +0530

    Add sound-effect + favicon + README files. Completion of Project.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Day 12 Challenge: Key Sequence Detection
+
+#### Live Site URL: https://basuabhirup.github.io/js30-day-12-key-sequence-detection/
+
+This is the project for the Day 12 of the 30-days vanilla JavaScript coding challenge from https://javascript30.com/
+
+
+![Screenshot of the completed Project](./screenshot-day12.png)
--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const sequenceArray = [];
 const secretCode = "abhirup";
 
+function playSound() {
+  sound = new Audio("flashback.mp3");
+  sound.play();
+}
+
 window.addEventListener("keyup", (e) => {
   sequenceArray.push(e.key);
   if(sequenceArray.length > secretCode.length){
     sequenceArray.shift();
   }
-  console.log(sequenceArray);
   if(sequenceArray.join("").includes(secretCode)) {
     cornify_add();
+    playSound();
   }
 })
